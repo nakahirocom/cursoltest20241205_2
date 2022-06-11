66e7d36cbcbf76909dadedb3a74fde514f157dcf
nakahirocom <31771074+nakahirocom@users.noreply.github.com>
2022-06-12 00:43:24 +0900

    santaku→sentakuへ変更

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express-santaku' });
+  res.render('index', { title: 'Express-sentaku' });
 });
 
 module.exports = router;
