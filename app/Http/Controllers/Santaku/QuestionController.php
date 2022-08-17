b6aeb5f7d9d13a65769112d9c807578b17d155ac
Shoma Nishitateno <shoma416@gmail.com>
2022-08-17 23:32:23 +0900

    選択肢を並び替えて表示する機能のリファクタリング

--- app/Http/Controllers/Santaku/QuestionController.php
+++ app/Http/Controllers/Santaku/QuestionController.php
@@ -16,32 +16,17 @@ class QuestionController extends Controller
      */
     public function __invoke(Request $request)
     {
-//mysqlからランダムに３つを取ってくる
-        $santaku = Santaku::inRandomOrder()->take(3)->get();
-//      echo print_r($santaku);
-//      dd($santaku);
+        // mysqlからランダムに３つを取ってくる
+        $questions = Santaku::inRandomOrder()->take(3)->get();
 
-// ループを止めるためのカウンター初期値設定
-$counter = 0;
-// 問題をランダムに並べるための$array[重複なしの1〜３ランダム数字]
-$array = range(1, 3);
-shuffle($array);
-$array = array_slice($array, 0, 3);
-print_r($array);
+        // ランダムにとってきた問題の配列に対して、先頭のものを問題とする
+        $question = $questions[0];
 
-        $seikai = $santaku[0];
-
-        $mondai1 = $santaku[$array[0]-1];
-        $mondai2 = $santaku[$array[1]-1];
-        $mondai3 = $santaku[$array[2]-1];
+        // 選択肢をランダムに並び替える
+        $shuffled = $questions->shuffle();
 
         return view('santaku.question')
-            ->with('seikai', $seikai)
-            ->with('mondai1', $mondai1)
-            ->with('mondai2', $mondai2)
-            ->with('mondai3', $mondai3)
-            ->with('santaku', $santaku)
-            ->with('counter', $counter);
-
+            ->with('question', $question)
+            ->with('questions', $shuffled);
         }
 }
--- resources/views/santaku/question.blade.php
+++ resources/views/santaku/question.blade.php
@@ -7,51 +7,52 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>santakuアプリ</title>
 </head>
+<body>
 
-<p>method-actionは </p>
+<p>method-actionは</p>
 <h1>viewのファイル名__question.blade.php[三択問題が出題される画面]</h1>
-<p>-----------------------------------------------------------------------------------------
+<p>-----------------------------------------------------------------------------------------</p>
 <br />
-<a class="btn" href="/">index画面へ戻る</a></p>
-<p>-----------------------------------------------------------------------------------------
+<a class="btn" href="/">index画面へ戻る</a>
+<p>-----------------------------------------------------------------------------------------</p>
 <br />
 <span>■問題:</span>
 <br />
 <div>
-@foreach($santaku as $santaku1)
-
-            <summary>
-              <p>問題{{ $santaku1->question }}</p>  
-            </summary>
-            @if($counter =1) 
-            @break
-                $counter++;
-            @endif
-@endforeach
-
+    <summary>
+        <p>{{ $question->question }}</p>
+    </summary>
+</div>
 <p>---------------------------------------------------------------- </p>
 <br />
 <span>■選択肢</span>
 
 <form action="{{ route('answer.index') }}" method="post">
 @csrf
-<input type="hidden" name="" value="">
-<label><input type="radio" name="sentaku1" value="1">{{ $mondai1->answer }}</label>
-<br />
-<label><input type="radio" name="sentaku2" value="2">{{ $mondai2->answer }}</label>
-<br />
-<label><input type="radio" name="sentaku3" value="3">{{ $mondai3->answer }}</label>
-<input type="submit" value="回答する">
+    <input type="hidden" name="question_id" value="{{ $question->id }}">
+    @foreach($questions as $question)
+        <label>
+            <input type="radio" name="choice_id" value="{{ $question->id }}">
+                {{ $question->answer }}
+        </label>
+        <br />
+    @endforeach
+    <input type="submit" value="回答する">
 </form>
 
 <br />
 
-<h2>▼▼▼▼▼※開発用の案内につきここから下は削除予定▼▼▼▼▼▼</h2>
-<ul>この画面からの移動先<li><a href="/">1.〇index画面へ /resources/views/santaku/index.blade.php</a></li>
-    <li>2.✕問題と答えを新規作成画面へ /resources/views/santaku/new.blade.php</li>
-    <li>3.✕あなたの三択設定画面へ /resources/views/santaku/santakuset.blade.php</a></li>
-    <li> <a href="/question">4.〇三択を解く画面へ /resources/views/santaku/question.blade.php</a></li>
-    <li>5.✕自分が作成した問題を一覧表示する画面へ /resources/views/santaku/list.blade.php</li>
-    <li>6.✕問題と答えを編集する画面へ /resources/views/santaku/edit.blade.php</li>
-</ul>
+<section>
+    <h2>▼▼▼▼▼※開発用の案内につきここから下は削除予定▼▼▼▼▼▼</h2>
+    <ul>この画面からの移動先
+        <li><a href="/">1.〇index画面へ /resources/views/santaku/index.blade.php</a></li>
+        <li>2.✕問題と答えを新規作成画面へ /resources/views/santaku/new.blade.php</li>
+        <li>3.✕あなたの三択設定画面へ /resources/views/santaku/santakuset.blade.php</li>
+        <li><a href="/question">4.〇三択を解く画面へ /resources/views/santaku/question.blade.php</a></li>
+        <li>5.✕自分が作成した問題を一覧表示する画面へ /resources/views/santaku/list.blade.php</li>
+        <li>6.✕問題と答えを編集する画面へ /resources/views/santaku/edit.blade.php</li>
+    </ul>
+</section>
 
+</body>
+</html>
