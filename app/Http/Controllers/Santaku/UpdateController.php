051ffb479f0861b2256a4c24d7d89b7ca14f07a4
nakahiro <nakahirocom@gmail.com>
2022-09-12 02:03:08 +0900

    laravel本p138[34]サービスクラスを使って自分作成のみ更新する記述追加

--- app/Http/Controllers/Santaku/UpdateController.php
+++ app/Http/Controllers/Santaku/UpdateController.php
@@ -5,8 +5,10 @@ namespace App\Http\Controllers\Santaku;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Santaku\UpdateRequest;
 use App\Models\Santaku;
+use App\Services\SantakuService;
 use Illuminate\Http\Request;
 use symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 class UpdateController extends Controller
 {
@@ -16,8 +18,20 @@ class UpdateController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function __invoke(UpdateRequest $request)
+    public function __invoke(UpdateRequest $request, SantakuService $santakuService)
     {
+        if (!$santakuService->checkOwnMondai($request->user()->id,
+            $request->id())) 
+            {
+                return redirect()
+                ->route('list')
+                ->with('feedback.success', "他のユーザーの問題は更新出来ません");
+        
+                throw new AccessDeniedHttpException();
+            }
+            
+
+    
     $santaku = Santaku::where('id', $request->id())->firstOrFail();
     $santaku->question = $request->question();
     $santaku->answer = $request->answer();
