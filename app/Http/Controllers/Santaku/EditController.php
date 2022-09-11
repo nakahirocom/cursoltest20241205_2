179574cba26ef1b982fb269f9163990e80b349e7
nakahiro <nakahirocom@gmail.com>
2022-09-12 02:03:24 +0900

    laravel本p138[34]サービスクラスを使って自分作成のみ編集する記述追加

--- app/Http/Controllers/Santaku/EditController.php
+++ app/Http/Controllers/Santaku/EditController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers\Santaku;
 
 use App\Http\Controllers\Controller;
 use App\Models\Santaku;
+use App\Services\SantakuService;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 class EditController extends Controller
 {
@@ -14,12 +16,20 @@ class EditController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function __invoke(Request $request)
+    public function __invoke(Request $request, SantakuService $santakuService)
     {
-    $santakuId = (int) $request->route('santakuId');
-    $santaku = Santaku::where('id', $santakuId)->firstOrFail();
+        $santakuId = (int) $request->route('santakuId');
+            if (!$santakuService->checkOwnMondai($request->user()->id, $santakuId)) 
+                {
+                    return redirect()
+                    ->route('list')
+                    ->with('feedback.success', "他のユーザーの問題は編集出来ません");
 
+                    throw new AccessDeniedHttpException();
+                }
 
-    return view('santaku.edit')->with('santaku',$santaku);
+        $santaku = Santaku::where('id', $santakuId)->firstOrFail();
+
+        return view('santaku.edit')->with('santaku',$santaku);
     }
 }
