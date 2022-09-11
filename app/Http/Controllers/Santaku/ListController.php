e972f1baeac98c3c0bc7a25d0caf54157d9db93a
nakahiro <nakahirocom@gmail.com>
2022-09-12 02:04:19 +0900

    laravel本p138[34]サービスクラスを使って自分作成のみ編集ボタンを押せる記述追加

--- app/Http/Controllers/Santaku/ListController.php
+++ app/Http/Controllers/Santaku/ListController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers\Santaku;
 
 use App\Http\Controllers\Controller;
 use App\Models\Santaku;
+use App\Services\SantakuService;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 class ListController extends Controller
 {
@@ -14,9 +16,11 @@ class ListController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function __invoke(Request $request)
-    {
+    public function __invoke(Request $request, SantakuService $santakuService)
+    {   
+        $santakuId = (int) $request->route('santakuId');
         $santaku = Santaku::orderBy('created_at', 'DESC')->get();
+   
         return view('santaku.list')
             ->with('santaku', $santaku);
     }
