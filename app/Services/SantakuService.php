68140aa08a0b780bd25f21a34c147fb969b2a6d1
nakahiro <nakahirocom@gmail.com>
2022-09-12 02:00:31 +0900

    laravel本p137[33]コード重複を避けるためserviceクラスにコード実装する

---
+++ app/Services/SantakuService.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Santaku;
+
+class SantakuService
+{
+ 
+
+  //自分の作成問題かをチェックするメソッド
+  public function checkOwnMondai(int $userId, int $santakuId): bool
+  {
+    $santaku = Santaku::where('id', $santakuId)->first();
+    if(!$santaku)
+    {
+      return false;
+    }
+
+    return $santaku->user_id === $userId;
+  }
+}
