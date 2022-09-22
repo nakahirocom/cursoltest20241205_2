f7910af3cd2384b45e74fae2b44db67ef9ad32eb
nakahiro <nakahirocom@gmail.com>
2022-09-23 01:19:20 +0900

    factoryと重複していたコードを削除し、factoryを経由してseederを使えるようにしました。

--- database/seeders/SantakuSeeder.php
+++ database/seeders/SantakuSeeder.php
@@ -5,17 +5,12 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
+use App\Models\Santaku;
 
 class SantakuSeeder extends Seeder
 {
     public function run()
     {
-        DB::table('santaku')->insert([
-            'question' => Str::random(10),
-            'answer' => Str::random(3),
-            'comment' => Str::random(20),
-            'created_at' => now(),
-            'updated_at' => now(),
-        ]);
+        Santaku::factory()->count(10)->create();
     }
 }
