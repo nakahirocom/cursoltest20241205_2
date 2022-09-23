ee5cd9a428751a6a650d1a9e9fe41db23dc98f7b
nakahiro <nakahirocom@gmail.com>
2022-09-23 22:49:30 +0900

    db上、passwordが11111111は変換されるため、seederのphpも変換されたものを登録するように変更しました

--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class UsersSeeder extends Seeder
 {
@@ -17,17 +18,23 @@ class UsersSeeder extends Seeder
         User::create([
             'name' => 'naka1 100%',
             'email' => 'naka1@aa',
-            'password' => 11111111,
+            'email_verified_at' => now(),
+            'password' => '$2y$10$8ZafXpVHLtBCNGOZsPc3w.aVGGvzcj7K72z4qgjoePXffT5DgRKFi', // 11111111
+            'remember_token' => Str::random(10),
         ]);
         User::create([
             'name' => 'naka2 75%',
             'email' => 'naka2@aa',
-            'password' => 11111111,
+            'email_verified_at' => now(),
+            'password' => '$2y$10$8ZafXpVHLtBCNGOZsPc3w.aVGGvzcj7K72z4qgjoePXffT5DgRKFi', // 11111111
+            'remember_token' => Str::random(10),
         ]);
         User::create([
             'name' => 'naka3 50%',
             'email' => 'naka3@aa',
-            'password' => 11111111,
+            'email_verified_at' => now(),
+            'password' => '$2y$10$8ZafXpVHLtBCNGOZsPc3w.aVGGvzcj7K72z4qgjoePXffT5DgRKFi', // 11111111
+            'remember_token' => Str::random(10),
         ]);
     }
 }
