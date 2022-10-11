a3f1c082bdeac29f024525f338bb2dbabcbb1b0e
nakahiro <nakahirocom@gmail.com>
2022-10-11 20:01:47 +0900

    分類=classificationを分類=lavelに変更＋主キーのidカラムで変更の影響あるものを修正した

--- database/migrations/2022_10_08_160952_add_colums_to_answer_results_table.php
+++ database/migrations/2022_10_08_160952_add_colums_to_answer_results_table.php
@@ -14,15 +14,15 @@ return new class extends Migration
     public function up()
     {
         Schema::table('answer_results', function (Blueprint $table) {
-            // answer_resultsテーブルにlarge_classification_idを追加
-            $table->unsignedBigInteger('large_classification_id')->after('answered_question_id');
-            // answer_resultsテーブルのlarge_classification_idカラムにlarge_classificationsテーブルのlarge_classification_idカラムを関連づける
-            $table->foreign('large_classification_id')->references('large_classification_id')->on('large_classifications');
-            // answer_resultsテーブルにmiddle_classification_idを追加
+            // answer_resultsテーブルにlarge_lavel_idを追加
+            $table->unsignedBigInteger('large_lavel_id')->after('answered_question_id');
+            // answer_resultsテーブルのlarge_lavel_idカラムにlarge_lavelsテーブルのidカラムを関連づける
+            $table->foreign('large_lavel_id')->references('id')->on('large_lavels');
 
-            $table->unsignedBigInteger('middle_classification_id')->after('large_classification_id');
-            // answer_resultsテーブルのmiddle_classification_idカラムにmiddle_classificationsテーブルのmiddle_classification_idカラムを関連づける
-            $table->foreign('middle_classification_id')->references('middle_classification_id')->on('middle_classifications');
+            // answer_resultsテーブルにmiddle_lavel_idを追加
+            $table->unsignedBigInteger('middle_lavel_id')->after('large_lavel_id');
+            // answer_resultsテーブルのmiddle_lavel_idカラムにmiddle_lavelsテーブルのidカラムを関連づける
+            $table->foreign('middle_lavel_id')->references('id')->on('middle_lavels');
         });
     }
 
