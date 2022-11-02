1936aa21d4d106440fcb5517bb124078879241eb
nakahiro <nakahirocom@gmail.com>
2022-11-03 01:14:42 +0900

    63-UI画面をBootstrap(cssのフレームワーク)で整え、整形済。

--- resources/views/santaku/answer.blade.php
+++ resources/views/santaku/answer.blade.php
@@ -6,59 +6,70 @@
     <meta name="viewport"
         content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+<!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
     <title>santakuアプリ</title>
 </head>
 
 <body>
-    <a class="btn" href="/">index画面へ戻る</a>
-    <a class="btn" href="/question">次の問題へ</a>
+    <div class="container">
+    <a class="btn btn-link" href="/">index画面へ戻る</a>
+    <a class="btn btn-link" href="/question">次の問題へ</a>
     @auth
-    <p>ようこそ、{{ Auth::user()->name }}さん</p>
-    <p>ユーザーidは、{{ Auth::user()->id }}です</p>
+
     @endauth
 
-    <p>-----------------------------------------------------------------------------------------</p>
-    <h2>三択アプリ　答え合わせ画面</h2>
-    <p>-----------------------------------------------------------------------------------------</p>
-    <span>■問題:</span>
-    <summary>
-        <p>{{ $questioned->question }}</p>
-    </summary>
+    <p class="h2">三択アプリ　答え合わせ</p>
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item"><span class="mark">{{ Auth::user()->name }}</span> がログイン中</li>
+          <li class="breadcrumb-item active" aria-current="page">ユーザーid{{ Auth::user()->id }}</li>
+        </ol>
+      </nav>
 
-    <span>■{{ Auth::user()->name }} の選択</span>
-    <br />
-    <summary>
-        {{ $choiced->answer }}
+    <span>出題された問題:</span>
+    <p class="h2"><span class="btn btn-outline-dark">{{ $questioned->question }}</span>
+    </p>
 
-        @if ($isCorrect)
-        ：正解
-        @else
-        ：不正解
-        @endif
-        <p>---------------------------------------------------------------- </p>
+    @if ($isCorrect)
+    <span class="btn btn-outline-primary">
+         {{ $choiced->answer }}：正解</span>
+    @else
+    <span class="btn btn-outline-danger">
+        {{ $choiced->answer }}：不正解</span>
+    @endif
+<p>-------------------------------------------------------</p>
+
+
+    </p>
+    </span>
+
+        <span class="placeholder col-12 placeholder-xs"></span>
         <br />
+
         @for ($i = 0; $i < count($viewModels); $i++) 
+  
             @if ($viewModels[$i]->isCorrect() )
-            <span>■選択肢{{ $i + 1 }}:{{ "正解" }}</span>
+            <span class="btn btn-outline-primary">■選択肢{{ $i + 1 }}：{{ "正解" }}</span>
             @else
-            <span>■選択肢{{ $i + 1 }}:{{ "不正解" }}</span>
+            <span class="btn btn-outline-danger">■選択肢{{ $i + 1 }}：{{ "不正解" }}</span>
             @endif
 
-            <summary>
-                <p>問題：{{ $viewModels[$i]->getQuestion() }}</p>
-            </summary>
-            <summary>
-                <p>答え：{{ $viewModels[$i]->getAnswer() }}</p>
-            </summary>
-            <summary>
-                <p>解説：{{ $viewModels[$i]->getComment() }}</p>
-            </summary>
-            <p>あなたの正解率：{{ $uidseikairituModels[$i] }}% /累計回答数：{{ $uidkaitousuuModels[$i] }}</p>
-            <p>みんなの正解率：{{ $allseikairituModels[$i] }}% /累計回答数：{{ $allkaitousuuModels[$i] }}</p>
-
-            @endfor
-
-
+              <div class="d-inline p-2 bd-highlight">あなたの正解率：{{ $uidseikairituModels[$i] }}% /累計回答数：{{ $uidkaitousuuModels[$i] }}</div>
+              <div class="d-sm-inline p-2 bd-highlight">みんなの正解率：{{ $allseikairituModels[$i] }}% /累計回答数：{{ $allkaitousuuModels[$i] }}</div>
+              <div class="collapse show" id="collapseExample" style="">
+                <div class="card card-body">
+                    <p>問題：{{ $viewModels[$i]->getQuestion() }}</p>
+                    <p>答え：{{ $viewModels[$i]->getAnswer() }}</p>
+                    <p>解説：{{ $viewModels[$i]->getComment() }}</p>
+                   Ï
+                </div>
+            </div>                  
+            <br>
+Ï
+        @endfor
+        </div>
 </body>
 
 </html>
--- resources/views/santaku/santakuset.blade.php
+++ resources/views/santaku/santakuset.blade.php
@@ -6,10 +6,15 @@
     <meta name="viewport"
         content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+<!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
     <title>santakuアプリ</title>
 </head>
+<body>
+    <div class="container">
 
-<a class="btn" href="/">index画面へ戻る</a>
+<a class="btn btn-link" href="/">index画面へ戻る</a>
 @auth
 <p>ようこそ、{{ Auth::user()->name }}さん</p>
 <p>ユーザーidは、{{ Auth::user()->id }}です</p>
@@ -18,7 +23,7 @@
 <p>-----------------------------------------------------------------------------------------</p>
 <h2>三択アプリ　{{ Auth::user()->name }}の出題範囲設定画面</h2>
 <p>-----------------------------------------------------------------------------------------</p>
-
+Ï
 <form method="get" action="/question">■この設定で問題をとく<br />
     <span>選択数:</span>
     <br />
@@ -43,3 +48,5 @@
     <br />
     <button type="submit">選択問題をとく</button>
 </form>
+</div>
+</body>
