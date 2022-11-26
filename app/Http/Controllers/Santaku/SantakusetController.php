39811d75af0228e826ca9bd190e8b16ef15a891a
nakahiro <nakahirocom@gmail.com>
2022-11-27 00:11:15 +0900

    omposhipsをインストールしたが、結局わからず。join句で複合外部キーのデータ取得。

--- app/Http/Controllers/Santaku/SantakusetController.php
+++ app/Http/Controllers/Santaku/SantakusetController.php
@@ -23,12 +23,13 @@ class SantakusetController extends Controller
                 $id = auth()->id();
                 //　不正解問題（正解idと解答idが不一致）で認証ユーザーの問題を新しい日時順でdbから抽出する
                 $incorrectList = AnswerResults::with('question')
-                        ->whereColumn('question_id', '!=', 'answered_question_id')
-                        ->where('user_id', '=', $id)
-                        ->orderBy('created_at', 'DESC')
+                        ->whereColumn('answer_results.question_id', '!=', 'answer_results.answered_question_id')
+                        ->where('answer_results.user_id', '=', $id)
+                        ->select('answer_results.id', 'answer_results.user_id', 'answer_results.question_id', 'answer_results.answered_question_id', 'answer_results.updated_at', 'questions.id as q_id', 'questions.question as q_question', 'questions.answer as q_answer', 'questions.comment as q_comment')
+                        ->join('questions', 'answer_results.answered_question_id', '=', 'questions.id')
+                        ->orderBy('answer_results.created_at', 'DESC')
                         ->get();
                 dump($incorrectList);
-
                 return view('santaku.santakuset')
                         ->with('incorrectList', $incorrectList);
         }
