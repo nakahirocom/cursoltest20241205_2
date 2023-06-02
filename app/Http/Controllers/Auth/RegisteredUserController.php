d713397b5ff6e4d6932e81fbf984efb0460a5d5c
nakahiro <nakahirocom@gmail.com>
2023-06-03 00:49:00 +0900

    新規登録したユーザーの場合、labelStoragesテーブルに中分類選択済としてselect=1を登録する。

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use App\Models\MiddleLabel;
+use App\Models\LabelStorages;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\Request;
@@ -49,6 +51,21 @@ class RegisteredUserController extends Controller
 
         Auth::login($user);
 
+        //新規登録した自身のuseridを取得する
+        $id = auth()->id();
+
+        //MiddleLabelテーブルの全てのデータ(中分類)を$middlelabelNewListに保存する
+        $middleLabelNewList = MiddleLabel::all();
+
+        //LabelStoragesテーブルの[user_id=全て自分の$id][large_label_id=ミドルラベルから][middle_label_id=全てのid][select=全て1]
+        foreach ($middleLabelNewList as $item) {
+            $selectNewList = new LabelStorages();
+            $selectNewList->user_id = $id; // ここでUserIdを保存している　❌ =$id();
+            $selectNewList->large_label_id = $item->large_label_id; //large_label_idを登録
+            $selectNewList->middle_label_id = $item->id; //middle_label_idを登録
+            $selectNewList->select = 1; //中分類を選んだ状態の「１」を登録
+            $selectNewList->save();
+        }
         return redirect(RouteServiceProvider::HOME);
     }
 }
