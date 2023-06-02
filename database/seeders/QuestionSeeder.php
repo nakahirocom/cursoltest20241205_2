b871af96d622d80134ddcec8cbea63712249be64
nakahiro <nakahirocom@gmail.com>
2023-06-03 00:41:37 +0900

    seederでanswerにジャンルを見分けつくように文章追加。

--- database/seeders/QuestionSeeder.php
+++ database/seeders/QuestionSeeder.php
@@ -12,144 +12,216 @@ class QuestionSeeder extends Seeder
         Question::create([
             'user_id' => 1,
             'question' => '1×1=',
-            'answer' => 1,
+            'answer' => '1  ：掛け算ジャンルのanswer',
             'comment' => 'イチカケルイチハイチ',
             'large_label_id' => 1,
-            'middle_label_id' => 1,
+            'middle_label_id' => 3,
         ]);
         Question::create([
             'user_id' => 1,
             'question' => '1×2=',
-            'answer' => 2,
+            'answer' => '2  ：掛け算ジャンルのanswer',
             'comment' => 'イチカケルニハニ',
             'large_label_id' => 1,
-            'middle_label_id' => 1,
+            'middle_label_id' => 3,
         ]);
         Question::create([
             'user_id' => 1,
             'question' => '1×3=',
-            'answer' => 3,
+            'answer' => '3  ：掛け算ジャンルのanswer',
             'comment' => 'イチカケルサンハサン',
             'large_label_id' => 1,
-            'middle_label_id' => 1,
+            'middle_label_id' => 3,
         ]);
         Question::create([
             'user_id' => 1,
             'question' => '1×4=',
-            'answer' => 4,
+            'answer' => '4  ：掛け算ジャンルのanswer',
             'comment' => 'イチカケルヨンハヨン',
             'large_label_id' => 1,
-            'middle_label_id' => 1,
+            'middle_label_id' => 3,
         ]);
         Question::create([
             'user_id' => 1,
             'question' => '1×5=',
-            'answer' => 5,
+            'answer' => '5  ：掛け算ジャンルのanswer',
             'comment' => 'イチカケルゴハゴ',
             'large_label_id' => 1,
-            'middle_label_id' => 1,
+            'middle_label_id' => 3,
         ]);
         Question::create([
             'user_id' => 1,
             'question' => '1×6=',
-            'answer' => 6,
+            'answer' => '6  ：掛け算ジャンルのanswer',
             'comment' => 'イチカケルロクハロク',
             'large_label_id' => 1,
-            'middle_label_id' => 1,
+            'middle_label_id' => 3,
         ]);
         Question::create([
             'user_id' => 1,
             'question' => '1×7=',
-            'answer' => 7,
+            'answer' => '7  ：掛け算ジャンルのanswer',
             'comment' => 'イチカケルナナハナナ',
             'large_label_id' => 1,
-            'middle_label_id' => 1,
+            'middle_label_id' => 3,
         ]);
         Question::create([
             'user_id' => 1,
             'question' => '1×8=',
-            'answer' => 8,
+            'answer' => '8  ：掛け算ジャンルのanswer',
             'comment' => 'イチカケルハチハハチ',
+            'large_label_id' => 1,
+            'middle_label_id' => 3,
+        ]);
+        Question::create([
+            'user_id' => 1,
+            'question' => '1×9=',
+            'answer' => '9  ：掛け算ジャンルのanswer',
+            'comment' => 'イチカケルキュウハキュウ',
+            'large_label_id' => 1,
+            'middle_label_id' => 3,
+        ]);
+        Question::create([
+            'user_id' => 2,
+            'question' => '1+1=',
+            'answer' => '2  ：足し算ジャンルのanswer',
+            'comment' => 'イチタスイチハニ',
+            'large_label_id' => 1,
+            'middle_label_id' => 1,
+        ]);
+        Question::create([
+            'user_id' => 2,
+            'question' => '1+2=',
+            'answer' => '3  ：足し算ジャンルのanswer',
+            'comment' => 'イチタスニハサン',
+            'large_label_id' => 1,
+            'middle_label_id' => 1,
+        ]);
+        Question::create([
+            'user_id' => 2,
+            'question' => '1+3=',
+            'answer' => '4  ：足し算ジャンルのanswer',
+            'comment' => 'イチタスサンハヨン',
+            'large_label_id' => 1,
+            'middle_label_id' => 1,
+        ]);
+        Question::create([
+            'user_id' => 2,
+            'question' => '1+4=',
+            'answer' => '5  ：足し算ジャンルのanswer',
+            'comment' => 'イチタスヨンハゴ',
+            'large_label_id' => 1,
+            'middle_label_id' => 1,
+        ]);
+        Question::create([
+            'user_id' => 2,
+            'question' => '1+5=',
+            'answer' => '6  ：足し算ジャンルのanswer',
+            'comment' => 'イチタスゴハロク',
+            'large_label_id' => 1,
+            'middle_label_id' => 1,
+        ]);
+        Question::create([
+            'user_id' => 2,
+            'question' => '1+6=',
+            'answer' => '7  ：足し算ジャンルのanswer',
+            'comment' => 'イチタスロクハナナ',
+            'large_label_id' => 1,
+            'middle_label_id' => 1,
+        ]);
+        Question::create([
+            'user_id' => 2,
+            'question' => '1+7=',
+            'answer' => '8  ：足し算ジャンルのanswer',
+            'comment' => 'イチタスナナハハチ',
+            'large_label_id' => 1,
+            'middle_label_id' => 1,
+        ]);
+        Question::create([
+            'user_id' => 2,
+            'question' => '1+8=',
+            'answer' => '9  ：足し算ジャンルのanswer',
+            'comment' => 'イチタスハチハキュウ',
+            'large_label_id' => 1,
+            'middle_label_id' => 1,
+        ]);
+        Question::create([
+            'user_id' => 2,
+            'question' => '1+9=',
+            'answer' => '10 ：足し算ジャンルのanswer',
+            'comment' => 'イチタスキュウハジュウ',
             'large_label_id' => 1,
             'middle_label_id' => 1,
         ]);
         Question::create([
             'user_id' => 1,
-            'question' => '1×9=',
-            'answer' => 9,
-            'comment' => 'イチカケルキュウハキュウ',
-            'large_label_id' => 1,
-            'middle_label_id' => 1,
-        ]);
-        Question::create([
-            'user_id' => 2,
-            'question' => '2×1=',
-            'answer' => 2,
-            'comment' => 'ニカケルイチハニ',
+            'question' => '1-1=',
+            'answer' => '0  ：引き算ジャンルのanswer',
+            'comment' => 'イチヒクイチハ',
             'large_label_id' => 1,
             'middle_label_id' => 2,
         ]);
         Question::create([
-            'user_id' => 2,
-            'question' => '2×2=',
-            'answer' => 4,
-            'comment' => 'ニカケルニハヨン',
+            'user_id' => 1,
+            'question' => '2-1=',
+            'answer' => '1  ：引き算ジャンルのanswer',
+            'comment' => 'ニヒクイチハ',
             'large_label_id' => 1,
             'middle_label_id' => 2,
         ]);
         Question::create([
-            'user_id' => 2,
-            'question' => '2×3=',
-            'answer' => 6,
-            'comment' => 'ニカケルサンハロク',
+            'user_id' => 1,
+            'question' => '3-1=',
+            'answer' => '2  ：引き算ジャンルのanswer',
+            'comment' => 'サンヒクイチハ',
             'large_label_id' => 1,
             'middle_label_id' => 2,
         ]);
         Question::create([
-            'user_id' => 2,
-            'question' => '2×4=',
-            'answer' => 8,
-            'comment' => 'ニカケルヨンハハチ',
+            'user_id' => 1,
+            'question' => '4-1=',
+            'answer' => '3  ：引き算ジャンルのanswer',
+            'comment' => 'ヨンヒクイチハ',
             'large_label_id' => 1,
             'middle_label_id' => 2,
         ]);
         Question::create([
-            'user_id' => 2,
-            'question' => '2×5=',
-            'answer' => 10,
-            'comment' => 'ニカケルゴハジュウ',
+            'user_id' => 1,
+            'question' => '5-1=',
+            'answer' => '4  ：引き算ジャンルのanswer',
+            'comment' => 'ゴヒクイチハ',
             'large_label_id' => 1,
             'middle_label_id' => 2,
         ]);
         Question::create([
-            'user_id' => 2,
-            'question' => '2×6=',
-            'answer' => 12,
-            'comment' => 'ニカケルロクハジュウニ',
+            'user_id' => 1,
+            'question' => '6-1=',
+            'answer' => '5  ：引き算ジャンルのanswer',
+            'comment' => 'ロクヒクイチハ',
             'large_label_id' => 1,
             'middle_label_id' => 2,
         ]);
         Question::create([
-            'user_id' => 2,
-            'question' => '2×7=',
-            'answer' => 14,
-            'comment' => 'ニカケルナナハジュウヨン',
+            'user_id' => 1,
+            'question' => '7-1=',
+            'answer' => '6  ：引き算ジャンルのanswer',
+            'comment' => 'ナナヒクイチハ',
             'large_label_id' => 1,
             'middle_label_id' => 2,
         ]);
         Question::create([
-            'user_id' => 2,
-            'question' => '2×8=',
-            'answer' => 16,
-            'comment' => 'ニカケルハチハジュウロク',
+            'user_id' => 1,
+            'question' => '8-1=',
+            'answer' => '7  ：引き算ジャンルのanswer',
+            'comment' => 'ハチヒクイチハ',
             'large_label_id' => 1,
             'middle_label_id' => 2,
         ]);
         Question::create([
-            'user_id' => 2,
-            'question' => '2×9=',
-            'answer' => 18,
-            'comment' => 'ニカケルキュウハジュウハチ',
+            'user_id' => 1,
+            'question' => '9-1=',
+            'answer' => '8  ：引き算ジャンルのanswer',
+            'comment' => 'キュウウヒクイチハ',
             'large_label_id' => 1,
             'middle_label_id' => 2,
         ]);
