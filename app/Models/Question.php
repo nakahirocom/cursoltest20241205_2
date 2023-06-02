7d6e8ad032577d0629e224d02acd694d8da29139
nakahiro <nakahirocom@gmail.com>
2023-06-03 00:55:46 +0900

    ユーザーが選択したジャンルからランダムに出題し、選択肢も出題された問題のジャンルの統一したもの

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -18,11 +18,34 @@ class Question extends Model
      */
     public static function getThreeQuestionsAtRandom(): array
     {
-        $q1 = Question::inRandomOrder()->first();
-        $q2 = Question::whereNot('answer', $q1?->answer)->inRandomOrder()->first();
-        $q3 = Question::whereNotIn('answer', [$q1?->answer, $q2?->answer])->inRandomOrder()->first();
-        $q4 = Question::whereNotIn('answer', [$q1?->answer, $q2?->answer, $q3?->answer])->inRandomOrder()->first();
-        $q5 = Question::whereNotIn('answer', [$q1?->answer, $q2?->answer, $q3?->answer, $q4?->answer])->inRandomOrder()->first();
+
+        $id = auth()->id();
+
+        $q1 = Question::
+            //ユーザーが選んでいるジャンルの内、（LabelStoragesのselectカラムが１）をwhereinの検索条件にinRandomOrder()しfirst()で１つ取得する。
+            wherein('middle_label_id', LabelStorages::where('user_id', $id)->where('select', 1)->select('middle_label_id'))
+            ->inRandomOrder()
+            ->first();
+
+        $q2 = Question::
+            //$q1でランダムで選択されたジャンルと同じジャンルをwhereで範囲指定し、$q1のanswerと答えが被らないものをランダムに１つ取得する。
+            where('middle_label_id', ($q1->middle_label_id))
+            ->whereNot('answer', $q1?->answer)->inRandomOrder()->first();
+
+        $q3 = Question::
+            //$q1でランダムで選択されたジャンルと同じジャンルをwhereで範囲指定し、$q1,$q2のanswerと答えが被らないものをランダムに１つ取得する。
+            where('middle_label_id', ($q1->middle_label_id))
+            ->whereNotIn('answer', [$q1?->answer, $q2?->answer])->inRandomOrder()->first();
+
+        $q4 = Question::
+            //$q1でランダムで選択されたジャンルと同じジャンルをwhereで範囲指定し、$q1,$q2,$q3のanswerと答えが被らないものをランダムに１つ取得する。
+            where('middle_label_id', ($q1->middle_label_id))
+            ->whereNotIn('answer', [$q1?->answer, $q2?->answer, $q3?->answer])->inRandomOrder()->first();
+
+        $q5 = Question::
+            //$q1でランダムで選択されたジャンルと同じジャンルをwhereで範囲指定し、$q1,$q2,$q3,$q4のanswerと答えが被らないものをランダムに１つ取得する。
+            where('middle_label_id', ($q1->middle_label_id))
+            ->whereNotIn('answer', [$q1?->answer, $q2?->answer, $q3?->answer, $q4?->answer])->inRandomOrder()->first();
 
         return [$q1, $q2, $q3, $q4, $q5];
     }
