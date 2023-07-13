ad8ccba496e9dde216650d92f524ca96ec44e9d8
nakahiro <nakahirocom@gmail.com>
2023-07-13 12:29:57 +0900

    insert-sqlsのファイル登録

---
+++ database/sqls/insert-large_labels.sql
@@ -0,0 +1,10 @@
+insert into large_labels (large_label, created_at, updated_at) values ('ひかり税理士', now(), now());
+insert into large_labels (large_label, created_at, updated_at) values ('必要な資格', now(), now());
+insert into large_labels (large_label, created_at, updated_at) values ('システム系', now(), now());
+insert into large_labels (large_label, created_at, updated_at) values ('その他', now(), now());
+insert into large_labels (large_label, created_at, updated_at) values ('予備1', now(), now());
+insert into large_labels (large_label, created_at, updated_at) values ('予備2', now(), now());
+insert into large_labels (large_label, created_at, updated_at) values ('予備3', now(), now());
+insert into large_labels (large_label, created_at, updated_at) values ('予備4', now(), now());
+insert into large_labels (large_label, created_at, updated_at) values ('予備5', now(), now());
+insert into large_labels (large_label, created_at, updated_at) values ('予備6', now(), now());
