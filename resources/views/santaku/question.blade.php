cc3d6759fd23193a0244e231d75da15a409c0987
nakahiro <nakahirocom@gmail.com>
2023-10-29 01:29:31 +0900

    ui画面修正_文字を黒に変更

--- resources/views/santaku/question.blade.php
+++ resources/views/santaku/question.blade.php
@@ -69,7 +69,7 @@
                 <div>
                     <button type="button" value="{{ $question_a->answer }}" id="{{ $question_a->id }}"
                         onclick="buttonClick('{{ $question_a->id }}')"
-                        class="sentaku rounded-md px-3.5 py-2 m-1 overflow-hidden relative group cursor-pointer border-2 font-medium border-indigo-600 text-indigo-600 text-white">
+                        class="sentaku rounded-md px-3.5 py-2 m-1 overflow-hidden relative group cursor-pointer border-2 font-medium border-indigo-600 text-indigo-600 text-black">
                         <span
                             class="absolute w-64 h-0 transition-all duration-300 origin-center rotate-45 -translate-x-20 bg-indigo-600 top-1/2 group-hover:h-64 group-hover:-translate-y-32 ease"></span>
                         <span class="relative text-indigo-600 transition duration-300 group-hover:text-white ease">
@@ -78,7 +78,7 @@
                     </button>
                 </div>
                 <div>　</div>
-                <div id=""> ◯問目の表示スペース</div>
+                <div id="{{ $question_a->id }}"> ◯問目の表示スペース</div>
             </div>
             @endforeach
 
