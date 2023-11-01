8fdedc368c9567bc9a6cacca4cada229af3f92c4
nakahiro <nakahirocom@gmail.com>
2023-11-01 23:46:21 +0900

    選択肢ボタンを一度押したら押せなくなるDisabled を追加

--- resources/views/santaku/question.blade.php
+++ resources/views/santaku/question.blade.php
@@ -67,9 +67,10 @@
                 <div>
                     <button type="button" value="{{ $question_a->answer }}" id="{{ $question_a->id }}"
                         onclick="buttonClick({{ $question_a->id }})"
-                        class="sentaku rounded-md px-3.5 py-2 m-1 overflow-hidden relative group cursor-pointer border-2 font-medium border-indigo-600 text-indigo-600 text-black">
+                        class="sentaku rounded-lg border border-primary-500 bg-primary-500 px-5 py-2.5 text-left text-sm font-medium text-white shadow-sm transition-all hover:border-primary-700 hover:bg-primary-700 focus:ring focus:ring-primary-200 disabled:cursor-not-allowed disabled:border-primary-300 disabled:bg-primary-300">
                         <span
-                            class="absolute w-64 h-0 transition-all duration-300 origin-center rotate-45 -translate-x-20 bg-indigo-600 top-1/2 group-hover:h-64 group-hover:-translate-y-32 ease"></span>
+                            class="absolute w-64 h-0 transition-all duration-300 origin-center rotate-45 -translate-x-20 bg-indigo-600 top-1/2 group-hover:h-64 group-hover:-translate-y-32 ease　hover:to-blue-300 
+                            text-white shadow-lg"></span>
                         <span class="relative text-indigo-600 transition duration-300 group-hover:text-white ease">
                              {{$question_a->answer }}</span>
 
