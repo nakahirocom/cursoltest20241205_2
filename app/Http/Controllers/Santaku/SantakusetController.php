17d7259a6993da673856fdab955ca3cf06226f59
nakahiro <nakahirocom@gmail.com>
2024-04-10 00:26:10 +0900

    非表示の答え合わせボタンを押したらエラーが出るバグを修正

--- app/Http/Controllers/Santaku/SantakusetController.php
+++ app/Http/Controllers/Santaku/SantakusetController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\LabelStorages;
 use App\Models\LargeLabel;
 use App\Models\MiddleLabel;
+use App\Models\SmallLabel;
 
 use Illuminate\Http\Request;
 
@@ -19,11 +20,33 @@ class SantakusetController extends Controller
      */
     public function __invoke(Request $request)
     {
+
         // 現在認証しているユーザーのIDを取得
         $id = auth()->id();
+
         //　ログインしたユーザーの選んだジャンルを呼び出し、Eagerロードのためにwith([ミドルラベル、ラージラベル])してdbへのアクセスを少なくする
         $selectList = LabelStorages::where('user_id', $id)->with('smallLabel.middleLabel.largeLabel')->get();
-        //        dump($selectList);
+
+        $smalelabelList = SmallLabel::all();
+
+
+// 両方のリストの数を比較
+// $selectListと$smalelabelListの数が異なる、かつ$selectListにない$smalelabelListの要素を追加
+if ($selectList->count() != $smalelabelList->count()) {
+    foreach ($smalelabelList as $item) {
+        // $selectListに$itemが含まれていないかチェック
+        if (!$selectList->contains('small_label_id', $item->id)) {
+            $selectNewList = new LabelStorages();
+            $selectNewList->user_id = $id; // UserIdを保存
+            $selectNewList->small_label_id = $item->id; // small_label_idを登録
+            $selectNewList->selected = 1; // 中分類を選んだ状態の「1」を登録
+            $selectNewList->save();
+        }
+    }
+} else {
+    // $selectListと$smalelabelListの数が同じ場合の処理
+    // ここに必要なコードを記述します
+}
         $largelabelList = LargeLabel::all();
         $middlelabelList = MiddleLabel::all();
 
