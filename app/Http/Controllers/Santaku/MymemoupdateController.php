55717ba81a12bbd899ef22e3721f0f67436070ae
nakahiro <nakahirocom@gmail.com>
2024-05-07 01:31:22 +0900

    私のメモ機能追加

--- app/Http/Controllers/Santaku/MymemoupdateController.php
+++ app/Http/Controllers/Santaku/MymemoupdateController.php
@@ -9,8 +9,6 @@ use App\Http\Requests\Santaku\MymemoupdateRequest;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use App\Models\Mymemo;
 
-
-
 class MymemoupdateController extends Controller
 {
     /**
@@ -19,14 +17,18 @@ class MymemoupdateController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function __invoke(MymemoupdateRequest $request, SantakuService $santakuService)
+    public function __invoke(MymemoupdateRequest $request, $questionId)
     {
+        // 現在認証しているユーザーのIDを取得
+        $id = auth()->id();
 
-        // データベースから私のメモを取得し、更新する
-        $mymemo = Mymemo::where('question_id', $request->user()->id)->firstOrFail();
-        $mymemo->mymemo = $request->mymemo;
-        $mymemo->save();
+        // user_id と question_id で Mymemo レコードを探し、なければ新規作成またはあれば更新
+        $mymemo = Mymemo::updateOrCreate(
+            ['user_id' => $id, 'question_id' => $questionId], // 検索条件
+            ['mymemo' => $request->mymemo] // 更新または新規作成時にセットする値
+        );
 
+        // リダイレクトとフィードバックメッセージ
         return redirect()
             ->route('mymemo', ['questionId' => $mymemo->id])
             ->with('feedback.success', '私のメモを更新しました');
