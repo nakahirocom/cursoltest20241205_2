87a3a275dfba1e9aad9c78604164798fda479127
nakahiro <nakahirocom@gmail.com>
2024-05-07 01:31:05 +0900

    私のメモ追加機能に伴うfillable設定

--- app/Models/Mymemo.php
+++ app/Models/Mymemo.php
@@ -10,6 +10,12 @@ class Mymemo extends Model
     use HasFactory;
     protected $table = 'mymemos';
 
+    protected $fillable = [
+        'user_id',
+        'mymemo',
+        'question_id',  // この行を追加
+    ];
+
 
     public function user()
     {
